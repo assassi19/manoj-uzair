bd09deaf6062b80236c24f5c36202cc61e6060ce
uzair <106462684+assassi19@users.noreply.github.com>
2023-03-31 09:42:55 +0530

    manoj.cpp
    this is where we'll rewrite our codes

---
+++ testing.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main(){
+   cout<<"hello manoj,"<<endl;
+   cout<<"try adding new lines here"<<endl;
+return 0;
+}
